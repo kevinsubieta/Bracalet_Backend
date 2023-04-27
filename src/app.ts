0ea363f67b90a3e852e6defd626b30a2ff75efba
kevin <kevinsubieta@hotmail.com>
2023-04-27 14:05:36 -0400

    New endpoints for services

--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,12 @@ const bodyParser = require('body-parser')
 dotenv.config();
 
 const app = express();
+const cors = require('cors')
+
 app.use(express.json());
 app.use(bodyParser.urlencoded())
 app.use(bodyParser.json())
+app.use(cors())
 app.use(indexRoutes);
 
 
