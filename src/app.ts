3a2026cdc565f9838153d678b36df5d99002fb00
kevin <kevinsubieta@hotmail.com>
2023-05-08 21:00:39 -0400

    Try new options 2.

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,14 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import indexRoutes from './service/index.routes';
 const bodyParser = require('body-parser')
+const cors = require('cors')
 dotenv.config();
 
 const app = express();
 
+
+
+app.use(cors())
 app.use(express.json());
 app.use(bodyParser.urlencoded())
 app.use(bodyParser.json())
